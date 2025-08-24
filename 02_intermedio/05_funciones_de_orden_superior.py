b107ccd7f1184652be1502f954f3fde65788d663
dino.maynasa.maydana@gmail.com <dino.maynasa.maydana@gmail.com>
2025-08-23 20:41:33 -0400

    funciones por orden superior

--- 02_intermedio/05_funciones_de_orden_superior.py
+++ 02_intermedio/05_funciones_de_orden_superior.py
@@ -1,1 +1,12 @@
-#
+#funciones jerarquicos
+''''
+En Python, las funciones de orden superior (higher-order functions) son aquellas que pueden recibir otras funciones como argumentos o devolver funciones como resultado.
+'''
+def potencia(n):
+    return lambda x: x ** n
+
+cuadrado = potencia(2)
+cubo = potencia(3)
+
+print(cuadrado(4))  # → 16
+print(cubo(2))      # → 8
